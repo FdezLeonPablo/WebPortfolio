8702166818e718f306f29f38eeb0743443a3d58b
leonpablo <fdezleon.pablo@gmail.com>
2024-10-18 23:05:47 +0200

    fixed minor details in the Header and Menu

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,8 +1,7 @@
-import ArrowRightIcon from '@/app/assets/arrow-right-s-line.svg';
-import ArrowDownIcon from '@/app/assets/arrow-down-s-line.svg';
-import CloseMenu from '@/app/assets/close-line.svg';
-import FolderIcon from '@/app/assets/folder-3-fill.svg';
-import { useState } from 'react';
+import ArrowRightIcon from "@/app/assets/arrow-right-s-line.svg";
+import ArrowDownIcon from "@/app/assets/arrow-down-s-line.svg";
+import FolderIcon from "@/app/assets/folder-3-fill.svg";
+import { useState } from "react";
 
 interface FolderOrFile {
   name: string;
@@ -12,42 +11,36 @@ interface FolderOrFile {
 
 const folderStructure: FolderOrFile[] = [
   {
-    name: '_hello',
-    files: [
-      { name: 'you-are-here' },
-    ],
+    name: "_hello",
+    files: [{ name: "you-are-here" }],
   },
   {
-    name: '_about-me',
-    files: [
-      { name: 'trajectory' },
-      { name: 'skills' },
-      { name: 'hobbies' },
-    ],
+    name: "_about-me",
+    files: [{ name: "trajectory" }, { name: "skills" }, { name: "hobbies" }],
   },
   {
-    name: '_projects',
-    files: [
-      { name: 'stunning-saas' },
-    ],
+    name: "_projects",
+    files: [{ name: "stunning-saas" }],
   },
   {
-    name: '_contact-me',
-    files: [
-      { name: 'contact-card' },
-    ],
+    name: "_contact-me",
+    files: [{ name: "contact-card" }],
   },
 ];
 
 const folderColors: Record<string, string> = {
-    '_hello': 'fill-[#FEA55F]',
-    '_about-me': 'fill-[#43D9AD]',
-    '_projects': 'fill-[#E99287]',
-    '_contact': 'fill-[#C98BDF]',
-  };
+  _hello: "fill-[#FEA55F]",
+  "_about-me": "fill-[#43D9AD]",
+  _projects: "fill-[#E99287]",
+  _contact: "fill-[#C98BDF]",
+};
 
-const Menu = () => {
-  const [openFolders, setOpenFolders] = useState<{ [key: string]: boolean }>({});
+const Menu = ({ isVisible }: { isVisible: boolean }) => {
+    const [openFolders, setOpenFolders] = useState<{ [key: string]: boolean }>(
+        
+        {}
+    );
+    if (!isVisible) return null;
 
   const toggleFolder = (folderName: string) => {
     setOpenFolders((prevState) => ({
@@ -59,14 +52,20 @@ const Menu = () => {
   return (
     <nav className="p-4 bg-primary-100">
       {folderStructure.map((folder) => (
-        <div key={folder.name} onClick={() => toggleFolder(folder.name)} className="cursor-pointer">
+        <div
+          key={folder.name}
+          onClick={() => toggleFolder(folder.name)}
+          className="cursor-pointer"
+        >
           <span className="flex items-center text-sm text-secondary-100 mb-1">
             {openFolders[folder.name] ? (
               <ArrowDownIcon className="w-4 h-4 mr-2" />
             ) : (
               <ArrowRightIcon className="w-4 h-4 mr-2" />
             )}
-            <FolderIcon className={`w-4 h-4 mr-2 ${folderColors[folder.name]}`} />
+            <FolderIcon
+              className={`w-4 h-4 mr-2 ${folderColors[folder.name]}`}
+            />
             {folder.name}
           </span>
           {openFolders[folder.name] && folder.files && (
--- src/app/sections/Header.tsx
+++ src/app/sections/Header.tsx
@@ -1,15 +1,29 @@
-'use client';
-import MenuIcon from '@/app/assets/menu-line.svg';
-import Menu from '@/app/components/Menu';
+"use client";
+import { useState } from "react";
+import MenuIcon from "@/app/assets/menu-line.svg";
+import Menu from "@/app/components/Menu";
+import CloseMenu from "@/app/assets/close-large-line.svg";
 
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="sticky top-0 border border-secondary-100 rounded-lg overflow-hidden">
       <div className="flex justify-between items-center bg-primary-100 px-4 py-4 text-sm border-b border-secondary-100">
-        <p className='text-secondary-100'>Pablo-Leon</p>
-        <MenuIcon className="h-6 w-6 text-secondary-100" />
+        <p className="text-secondary-100">Pablo-Leon</p>
+        <div onClick={toggleMenu} className="cursor-pointer">
+          {menuOpen ? (
+            <CloseMenu className="h-6 w-6 text-secondary-100" />
+          ) : (
+            <MenuIcon className="h-6 w-6 text-secondary-100" />
+          )}
+        </div>
       </div>
-      <Menu/>
+      <Menu isVisible={menuOpen}/>
     </header>
   );
 };
