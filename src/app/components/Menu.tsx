38b11ed49fbcea9cdec3efb5b688c23fbbb2574d
leonpablo <fdezleon.pablo@gmail.com>
2024-10-18 22:30:55 +0200

    Header basic structure and styles

---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,87 @@
+import ArrowRightIcon from '@/app/assets/arrow-right-s-line.svg';
+import ArrowDownIcon from '@/app/assets/arrow-down-s-line.svg';
+import CloseMenu from '@/app/assets/close-line.svg';
+import FolderIcon from '@/app/assets/folder-3-fill.svg';
+import { useState } from 'react';
+
+interface FolderOrFile {
+  name: string;
+  icon?: React.ComponentType;
+  files?: FolderOrFile[];
+}
+
+const folderStructure: FolderOrFile[] = [
+  {
+    name: '_hello',
+    files: [
+      { name: 'you-are-here' },
+    ],
+  },
+  {
+    name: '_about-me',
+    files: [
+      { name: 'trajectory' },
+      { name: 'skills' },
+      { name: 'hobbies' },
+    ],
+  },
+  {
+    name: '_projects',
+    files: [
+      { name: 'stunning-saas' },
+    ],
+  },
+  {
+    name: '_contact-me',
+    files: [
+      { name: 'contact-card' },
+    ],
+  },
+];
+
+const folderColors: Record<string, string> = {
+    '_hello': 'fill-[#FEA55F]',
+    '_about-me': 'fill-[#43D9AD]',
+    '_projects': 'fill-[#E99287]',
+    '_contact': 'fill-[#C98BDF]',
+  };
+
+const Menu = () => {
+  const [openFolders, setOpenFolders] = useState<{ [key: string]: boolean }>({});
+
+  const toggleFolder = (folderName: string) => {
+    setOpenFolders((prevState) => ({
+      ...prevState,
+      [folderName]: !prevState[folderName],
+    }));
+  };
+
+  return (
+    <nav className="p-4 bg-primary-100">
+      {folderStructure.map((folder) => (
+        <div key={folder.name} onClick={() => toggleFolder(folder.name)} className="cursor-pointer">
+          <span className="flex items-center text-sm text-secondary-100 mb-1">
+            {openFolders[folder.name] ? (
+              <ArrowDownIcon className="w-4 h-4 mr-2" />
+            ) : (
+              <ArrowRightIcon className="w-4 h-4 mr-2" />
+            )}
+            <FolderIcon className={`w-4 h-4 mr-2 ${folderColors[folder.name]}`} />
+            {folder.name}
+          </span>
+          {openFolders[folder.name] && folder.files && (
+            <ul className="pl-6 text-sm text-secondary-100">
+              {folder.files.map((file) => (
+                <li key={file.name} className="flex items-center mb-1">
+                  {file.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+export default Menu;
--- src/app/sections/Header.tsx
+++ src/app/sections/Header.tsx
@@ -1,13 +1,15 @@
+'use client';
 import MenuIcon from '@/app/assets/menu-line.svg';
-import { useState } from 'react';
+import Menu from '@/app/components/Menu';
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
-      <div className="flex justify-between items-center bg-primary-100 px-4 py-3 text-xs">
+    <header className="sticky top-0 border border-secondary-100 rounded-lg overflow-hidden">
+      <div className="flex justify-between items-center bg-primary-100 px-4 py-4 text-sm border-b border-secondary-100">
         <p className='text-secondary-100'>Pablo-Leon</p>
-        <MenuIcon className="h-5 w-5 text-secondary-100" />
+        <MenuIcon className="h-6 w-6 text-secondary-100" />
       </div>
+      <Menu/>
     </header>
   );
 };
